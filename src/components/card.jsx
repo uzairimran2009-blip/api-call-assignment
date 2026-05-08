3de87bb6be913b915a187a2b1508db565882ad7b
uzairimran2009-blip <uzair.imran2009@gmail.com>
2026-05-08 10:16:40 +0500

    My-Four-React-Project

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,43 +4,7 @@ const Card = (props) => {
 
     const { products } = props
 
-    // let discountedPrice = (
-    //     products.price -
-    //     (products.price * products.discountPercentage) / 100
-    // ).toFixed(2);
-
     return (
-        //     <div className="card">
-        //       <img src={product.thumbnail} alt={product.title} className="card-img" />
-
-        //       <div className="card-body">
-        //         <h2 className="title">{product.title}</h2>
-        //         <p className="brand">{product.brand}</p>
-
-        //         <p className="description">{product.description}</p>
-
-        //         <div className="price-section">
-        //           <span className="price">${discountedPrice}</span>
-        //           <span className="old-price">${product.price}</span>
-        //           <span className="discount">-{product.discountPercentage}%</span>
-        //         </div>
-
-        //         <div className="meta">
-        //           <span>⭐ {product.rating}</span>
-        //           <span>Stock: {product.stock}</span>
-        //         </div>
-
-        //         <button className="btn">Add to Cart</button>
-
-        // <div className="reviews">
-        //   <h4>Reviews</h4>
-        //   {product.reviews.map((review, index) => (
-        //     <div key={index} className="review">
-        //       <strong>{review.reviewerName}</strong>
-        //       <span> ⭐ {review.rating}</span>
-        //       <p>{review.comment}</p>
-        //     </div>
-
 
         <div className="container">
             <h1 className="heading">Product Store</h1>
@@ -62,38 +26,13 @@ const Card = (props) => {
                             </div>
 
                             <button className="btn">Add to Cart</button>
-                            {/* <img
-              src={item.thumbnail}
-              alt={item.title}
-              className="image"
-            />
 
-            <div className="card-body">
-              <h2 className="title">{item.title}</h2>
-              <p className="desc">{item.description}</p>
-
-              <div className="info">
-                <span className="price">${item.price}</span>
-                <span className="rating">⭐ {item.rating}</span>
-              </div>
-
-              <div className="stock">
-                {item.stock > 10 ? (
-                  <span className="in">In Stock</span>
-                ) : (
-                  <span className="low">Low Stock</span>
-                )}
-              </div>
-
-              <button className="btn">Add to Cart</button> */}
                         </div>
                     </div>
                 ))}
             </div>
         </div>
-        //   </div>
     );
-
 };
 
 export default Card;
